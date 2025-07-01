eb70c51ace036c6a338b0ee4568685266324f2d0
zumarRamadhan <zumar.ramadhan46@gmail.com>
2025-07-01 22:07:14 +0700

    fixing

--- bootstrap_dashboard.php
+++ bootstrap_dashboard.php
@@ -144,10 +144,7 @@
     </div>
   </div>
 
-  <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Logout Confirmation Script -->
   <script>
     document.getElementById('logoutBtn').addEventListener('click', function(e) {
       e.preventDefault();
